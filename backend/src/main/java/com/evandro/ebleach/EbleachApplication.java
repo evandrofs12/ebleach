9c21745f6851f48f67fd012b1586d87a14e95d19
evandrofs12 <evandrofs12@gmail.com>
2023-01-18 21:54:45 -0300

    backend: projeto spring boot criado

---
+++ backend/src/main/java/com/evandro/ebleach/EbleachApplication.java
@@ -0,0 +1,13 @@
+package com.evandro.ebleach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbleachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbleachApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/evandro/ebleach/EbleachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evandro.ebleach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbleachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
